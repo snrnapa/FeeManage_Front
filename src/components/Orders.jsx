01fb3fc2e608907b492963a7d56dd5fe08756c89
snrnapa <shino.satoru@gmail.com>
2023-06-24 22:53:48 +0900

    materialUIを使用してworker画面を実装した（APIFetchメソッドは、Order画面

---
+++ src/components/Orders.jsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import Link from '@mui/material/Link';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Title from './Title';
+import { useState , useEffect } from 'react';
+
+function preventDefault(event) {
+  event.preventDefault();
+}
+
+export default function Orders() {
+    const [workers , setWorkers] = useState([]);
+
+
+    useEffect(() => {
+        fetch("http://localhost:8080/", { method: "GET" })
+          .then(res => res.json())
+          .then(data => {
+            setWorkers(data)
+        })
+
+    },[]);
+
+
+  return (
+    <>
+      <Title>User List</Title>
+      <Table size="small" >
+        <TableHead>
+          <TableRow>
+            <TableCell>UserId</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Dept/Team</TableCell>
+
+
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {workers.map((worker) => (
+            <TableRow key={worker.id}>
+              <TableCell>{worker.id}</TableCell>
+              <TableCell>{worker.first_name} {worker.last_name}</TableCell>
+              <TableCell>{worker.dept} / {worker.team}</TableCell>
+
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
+        See more orders
+      </Link>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import Test from "./Test";
-import Inputer from "./Inputer";
-import Sidebar from "./components/Sidebar";
+
+import Orders from "./components/Orders";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -13,23 +11,10 @@ root.render(
 
     <div>
 
-      <Sidebar />
+      <Orders />
 
     </div>
 
-
-
-    <hr/>
-
-
-    <div>
-      <App />
-    </div>
-
-
-    <div>
-      <Test />
-    </div>
 
   </React.StrictMode>
 );
