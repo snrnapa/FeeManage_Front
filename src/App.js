c36af995d90c3fe2f98a54d176919abe3b80c99e
snrnapa <shino.satoru@gmail.com>
2023-06-24 14:26:58 +0900

    アイコンを追加してみた

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css";
 import ApiFetch from "./components/ApiFetch";
+import Fee from "./components/Fee";
+import PersonIcon from '@mui/icons-material/Person';
+import TrainIcon from '@mui/icons-material/Train';
+
 
 
 function App() {
   return (
     // <div className="App">
     <div>
+
+      <PersonIcon />
       <ApiFetch />
+
+      <hr/>
+      <TrainIcon />
+      <Fee />
+
 
       
     </div>
--- src/index.js
+++ src/index.js
@@ -9,15 +9,48 @@ import Sidebar from "./components/Sidebar";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+
+
     <div>
+<<<<<<< Updated upstream
       <Sidebar />
+=======
+      Sidebar
+        <Sidebar />
+>>>>>>> Stashed changes
     </div>
+
+
+
+    <hr/>
+
+
     <div>
       <App />
     </div>
 
+<<<<<<< Updated upstream
     <div>
       <Test />
     </div>
+=======
+
+    <hr/>
+
+    <div>
+      Test
+      <Test />
+    </div>
+    <hr/>
+
+      <div>
+        Inputer
+        <Inputer />
+      </div>
+    <hr/>
+
+    
+
+>>>>>>> Stashed changes
   </React.StrictMode>
 );
