ec27ae512b77b96d64a17f692d16e37b82a08daa
snrnapa <shino.satoru@gmail.com>
2023-07-02 17:23:17 +0900

    deptのボタンの重複を削除しました

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,17 +1,67 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import classNames from 'classnames';
 import { Button, ButtonGroup } from '@mui/material';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import { useState } from 'react';
 
 export const Filter = (props) => {
 
     // propsを定義  
   const { value, onChange } = props;
+  const [depts , setDepts] = useState([]);
   // フィルターの切り替え
   const handleClick = (key) => {
     
     onChange(key);
   }
+
+
+
+  useEffect(() => {
+
+    let deptlist = [];
+        
+    let newworkers = props.workers;
+
+    newworkers.map((worker) => {
+      if(!deptlist.includes(worker.dept)){
+
+        deptlist.push(worker.dept);
+        
+
+      }
+
+    
+    })
+    setDepts(deptlist);
+
+
+  },[])
+
+
+  const distincttest = () => {
+
+    
+    // let newworkers = props.workers;
+
+    // newworkers.map((worker) => {
+    //   if(!deptlist.includes(worker.dept)){
+
+    //     deptlist.push(worker.dept);
+
+    //   }
+
+
+    // })
+    
+    console.log(depts);
+
+
+
+
+  }
+
+
 
 
   return (
@@ -21,12 +71,13 @@ export const Filter = (props) => {
 
       <ButtonGroup>
 
-        {props.workers.map((worker) => (
-          <Button onClick={() => handleClick(worker.dept)}> {worker.dept} </Button>
+        {depts.map((dept) => (
+          <Button onClick={() => handleClick(dept)}> {dept} </Button>
         ))}
 
       </ButtonGroup>
 
+      <Button onClick={distincttest}>distinct</Button>
 
 
         
